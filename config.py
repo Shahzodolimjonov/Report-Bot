af8580a931bc215c49f29c2071498aaf89525816
Shahzod <shahzod555@gmail.com>
2025-07-27 17:21:51 +0500

    fix

--- config.py
+++ config.py
@@ -9,10 +9,8 @@ BOT_TOKEN = env.str('BOT_TOKEN')
 
 CHAT_ID = {
     'CHANNEL_ID1': env.str('CHANNEL_ID1'),
-    'CHANNEL_ID2': env.str('CHANNEL_ID2'),
-    'Limonpay Logs': env.str('CHAT_ID2'),
-    'Limonpay Test': env.str('CHAT_ID3'),
-    'sssss': env.str('CHAT_ID4'),
+    'sssss': env.str('CHAT_ID1'),
+    'test': env.str('CHAT_ID2'),
 }
 
 MAIN_CHAT_ID = env.str('MAIN_CHAT_ID')
--- handler.py
+++ handler.py
@@ -1,6 +1,5 @@
 from aiogram import Router, types, Bot, F
 from aiogram.filters import CommandStart
-
 from config import MAIN_CHAT_ID, BOT_TOKEN
 
 bot = Bot(token=BOT_TOKEN)
@@ -13,9 +12,7 @@ async def start_handler(message: types.Message):
 
 
 @router.message(F.text)
-async def handle_bot_message(message: types.Message):
-    print(message)
-
+async def handle_group_message(message: types.Message):
     if message.chat.type in {"group", "supergroup"}:
         group_name = message.chat.title
         chat_info = await bot.get_chat(message.chat.id)
@@ -25,25 +22,29 @@ async def handle_bot_message(message: types.Message):
             else "#"
         )
         chat_type = "Guruh"
-    elif message.chat.type == "channel":
-        group_name = message.chat.title
-        chat_info = await bot.get_chat(message.chat.id)
-        group_link = (
-            chat_info.invite_link if chat_info.invite_link
-            else f"https://t.me/{chat_info.username}" if chat_info.username
-            else "#"
+
+        pass_message = (
+            f"**Manba**: {chat_type}\n"
+            f"**Nomi**: [{group_name}]({group_link})\n"
+            f"**Xabar**: {message.text}\n"
         )
-        chat_type = "Kanal"
-        print(f"Kanal ID: {message.chat.id}")
-    else:
-        group_name = "Shaxsiy Chat"
-        group_link = "#"
-        chat_type = "Shaxsiy Chat"
+        await bot.send_message(MAIN_CHAT_ID, pass_message, parse_mode="Markdown", disable_web_page_preview=True)
+
+
+@router.channel_post()
+async def handle_channel_post(message: types.Message):
+    group_name = message.chat.title
+    chat_info = await bot.get_chat(message.chat.id)
+    group_link = (
+        chat_info.invite_link if chat_info.invite_link
+        else f"https://t.me/{chat_info.username}" if chat_info.username
+        else "#"
+    )
+    chat_type = "Kanal"
 
     pass_message = (
         f"**Manba**: {chat_type}\n"
         f"**Nomi**: [{group_name}]({group_link})\n"
         f"**Xabar**: {message.text}\n"
     )
-
     await bot.send_message(MAIN_CHAT_ID, pass_message, parse_mode="Markdown", disable_web_page_preview=True)
